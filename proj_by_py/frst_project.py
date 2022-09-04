d6ebdad471b31e93495855e15c22bc617d8ce25f
Movses30 <mseyranyan84@gmail.com>
2022-09-04 22:26:00 +0400

    das numbr 1

---
+++ proj_by_py/frst_project.py
@@ -0,0 +1,18 @@
+'''print("barev Movses")
+
+"""
+mi qni togh komment anelu hamar 
+"""
+
+#
+#print("barev MOvses")
+#print("vonc es"+" "+"Mvses jan ")
+#print(2.55)
+#print(2.55+3)
+#print("33"+"4") # 337
+IManun="Movse"
+f = "ok"
+print(f+"ooooooooooooooooo")
+print("hello"+IManun)
+
+'''

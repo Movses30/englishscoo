9f44821954a02f8f5c931411d3609a7c492292fb
Movses30 <mseyranyan84@gmail.com>
2022-09-05 00:10:08 +0400

    python das2

--- proj_by_py/frst_project.py
+++ proj_by_py/frst_project.py
@@ -16,3 +16,17 @@ print(f+"ooooooooooooooooo")
 print("hello"+IManun)
 
 '''
+''' inchpes sarqel ev ashxatel lissteri het <arr in js > 
+lit2list = [
+    [],  #incpes avelacnel value 0 indexin
+    [1,2,3,4,5,6,7,8,9],
+    ["m","o","vova","s","e","s"]
+]
+#print(lit2list[2][2]) #== vova
+print(lit2list)
+print("-------------------------------")
+#lit2list.append("avelacav")
+reslist = lit2list[0]+ lit2list[1]+ lit2list[2] 
+reslist.append("avelacav")    # listi megi arjeqner@ irar e kpcnum == [1, 2, 3, 4, 5, 6, 7, 8, 9, 'm', 'o', 'vova', 's', 'e', 's', 'avelacav']
+print(lit2list)
+print(reslist) '''
